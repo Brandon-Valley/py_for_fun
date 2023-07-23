3a8f5e9ff025054fec7f2cc3b71d5c3195742242
Brandon Valley <bavvh8@mst.edu>
2023-07-23 14:25:13 -0500

    init get_in_order_for_level() works

--- algo/binary_tree.py
+++ algo/binary_tree.py
@@ -29,9 +29,58 @@ def list_to_binary_tree(in_list) -> node:
 
     return get_node_with_children_from_index(0)
 
+# step 2
+def print_flattened_values(node):
+    def _print_min(node):
+        if not node:
+            return
+        
+        _print_min(node.left_node)
+        print(node.value, end=", ")
+        _print_min(node.right_node)
+
+    _print_min(node)
+
+
+# step 4
+def get_in_order_for_level(bt_root, level):
+    assert isinstance(bt_root, node)
+    assert isinstance(level, int)
+    assert level >= 0
+
+    def _get_result_list(cur_node = bt_root, cur_level = 0, result_list = None):
+        if not result_list:
+            result_list = []
+
+        if cur_level == level:
+            # print(cur_node.value, end=" ")
+            result_list.append(cur_node.value)
+            return result_list
+        
+        if cur_node.left_node:
+            result_list.extend(_get_result_list(cur_node.left_node, cur_level + 1))
+        if cur_node.right_node:
+            result_list.extend(_get_result_list(cur_node.right_node, cur_level + 1))
+
+        return result_list
+
+    return _get_result_list()
+
+
+
 
 
 test_list = [1,2,3,4,5,6,7]
 
-bt = list_to_binary_tree(test_list)
-print(bt)
+bt_root = list_to_binary_tree(test_list)
+print(bt_root)
+
+# step 2
+print(f"{print_flattened_values(bt_root)=}")
+
+
+print("\nSTEP 3 get_in_order_for_level()")
+print(f"{get_in_order_for_level(bt_root,0)=}")
+print(f"{get_in_order_for_level(bt_root,1)=}")
+print(f"{get_in_order_for_level(bt_root,2)=}")
+print(f"{get_in_order_for_level(bt_root,3)=}")
---
+++ little_tricks.py
@@ -0,0 +1,5 @@
+# alpha > int
+print(f"{int('a')=}")
+print(f"{int('z')=}")
+print(f"{int('A')=}")
+print(f"{int('A'.lower())=}")
